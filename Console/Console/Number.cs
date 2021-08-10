70c2db2d01b921a74114b1c7c957f74d3a7c47b2
Joao Oliveira <joao.oliveira@criticaltechworks.com>
2021-08-10 18:24:46 +0100

    Init commit 2

---
+++ Console/Console/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Console
+{
+    class Number
+    {
+        public  Random num = new Random();
+
+        public int GetRandomNumber()
+        {
+            return num.Next(1, 1);
+        }
+    }
+}
--- Console/Console/Program.cs
+++ Console/Console/Program.cs
@@ -5,7 +5,83 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+
+
+            //System.Console.WriteLine("Hello World!");
+            //int a = 3, i;
+            //for (i = 0; i < 250; i++) //prints love symbol 250 times
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+
+
+            //System.Console.WriteLine();
+            //for (i = 0; i < 75; i++)
+            //    System.Console.Write((char) a);
+            //for (i = 0; i < 20; i++)
+            //    System.Console.Write(" ");
+            //System.Console.Write("Olga");
+            //for (i = 0; i < 20; i++)
+            //    System.Console.Write(" ");
+            //for (i = 0; i < 75; i++)
+            //    System.Console.Write((char) 3);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+            //for (i = 0; i < 250; i++)
+            //    System.Console.Write((char) a);
+            //System.Console.WriteLine("");
+
+            var num = new Number();
+            int x = num.GetRandomNumber();
+            int y = x;
         }
+
+        //private static int num = Number.GetRandomNumber();
+        //private static int x = num;
+
+
+
     }
 }
+
+
+
+       
+
+
